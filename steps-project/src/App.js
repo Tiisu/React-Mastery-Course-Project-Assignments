82a762deae813aa4a4b017a7fd80fcfd77e42e32
Tiisu <tiisusharif@gmail.com>
2026-03-07 10:51:07 +0000

    feat: enhancing the steps app

--- steps-project/src/App.js
+++ steps-project/src/App.js
@@ -1,23 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+const messages = [
+  "Learn React ",
+  "Apply for jobs",
+  "Invest your new income"
+]
 
 function App() {
+
+  const [step, setStep] = useState(1)
+
+  function handleNext (){
+    if (step <3 ) {
+      // setStep(step +1)
+      setStep((c) => c +1)
+
+    }
+    
+  }
+
+  function handlePrev () {
+    if ( step >1) {
+      // setStep(step -1)
+      setStep((c) => c -1)
+    }
+    
+  }
+
+  // console.log(step)
+
+  // let step =1
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='steps'>
+
+        <div className='numbers'>
+          <div className={step >= 1? "active" : "inactive"}>1</div>
+          <div className={step >= 2? "active" : "inactive"}>2</div>
+          <div className={step >= 3? "active" : "inactive"}>3</div>
+        </div>
+
+        <div className='message'>
+          <div>STEP: {step} </div>
+          <div>{messages [step -1]}</div>
+          {/* <h1>Press Q</h1> */}
+
+        </div>
+
+        <div className='btns'>
+          <button onClick={handlePrev}>Previous</button>
+          <button onClick={handleNext}>Next</button>
+        </div>
+
+      </div>
+
+
+
     </div>
   );
 }
